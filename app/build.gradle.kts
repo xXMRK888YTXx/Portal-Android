31383751936ef565009b6faaabb478fa82944ef4
xXMRK888YTXx <aksehchikov@gmail.com>
2026-03-09 23:05:33 +0300

    [Chore] Increment versionCode to 7
    Updated the `versionCode` from 6 to 7 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.xxmrk888ytxx.portal"
         minSdk = catalogMinSdk
         targetSdk = catalogCompileSdk
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
