2bc63dccad377b693ce715801ce96f029f860cee
xXMRK888YTXx <aksehchikov@gmail.com>
2026-03-10 07:56:06 +0300

    [Chore] Update Parcelize plugin to use version catalog alias
    Updated the `kotlin-parcelize` plugin declaration in `app/build.gradle.kts` to use the version catalog alias format for consistency with other plugin declarations.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.serialization)
-    id("kotlin-parcelize")
+    alias(libs.plugins.kotlin.parcelize)
 }
 val catalogJavaVersion = libs.versions.jvm.target.get()
 val catalogCompileSdk = libs.versions.compile.sdk.get().toInt()
