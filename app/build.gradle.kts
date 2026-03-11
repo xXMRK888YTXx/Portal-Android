b0411e68e37ed000ea17acf984ca39a16097c312
xXMRK888YTXx <aksehchikov@gmail.com>
2026-03-11 22:42:56 +0300

    [Chore] Increment versionCode to 9
    Updated the `versionCode` from 8 to 9 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.xxmrk888ytxx.portal"
         minSdk = catalogMinSdk
         targetSdk = catalogCompileSdk
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
