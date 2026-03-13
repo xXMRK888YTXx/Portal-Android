5e7202d297a49e3dfb878401a8be764e4832d7f2
xXMRK888YTXx <aksehchikov@gmail.com>
2026-03-13 22:15:41 +0300

    [Chore] Increment versionCode to 11
    Updated the `versionCode` from 10 to 11 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.xxmrk888ytxx.portal"
         minSdk = catalogMinSdk
         targetSdk = catalogCompileSdk
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
