747f2311dd25fa6f8ac0cc85af8c1eeb0e890418
xXMRK888YTXx <aksehchikov@gmail.com>
2026-03-18 21:58:11 +0300

    [Fix] Update back navigation logic in `AddNewDeviceViewModel`
    This commit modifies the `previousPage` logic to ensure that navigating back from any page (other than the initial one) redirects the user specifically to the `SELECT_TYPE` page, rather than simply decrementing the page index.

--- AddNewDeviceScreen/src/main/java/com/xxmrk888ytxx/addnewdevicescreen/AddNewDeviceViewModel.kt
+++ AddNewDeviceScreen/src/main/java/com/xxmrk888ytxx/addnewdevicescreen/AddNewDeviceViewModel.kt
@@ -191,7 +191,7 @@ class AddNewDeviceViewModel @Inject constructor(
     private fun previousPage(currentPage: Page) {
         when (currentPage.id) {
             0 -> sendNavigationAction { navigateUp() }
-            else -> sideEffect.tryEmit(AddNewDeviceScreenSideEffect.ScrollToPage(currentPage.id - 1))
+            else -> sideEffect.tryEmit(AddNewDeviceScreenSideEffect.ScrollToPage(Page.SELECT_TYPE.id))
         }
     }
 
