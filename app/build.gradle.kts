48edb325723dd2321e27e6a6187265b3210f321e
xXMRK888YTXx <aksehchikov@gmail.com>
2026-03-21 20:19:18 +0300

    [Chore] Increment versionCode to 21
    Updated the `versionCode` from 20 to 21 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.xxmrk888ytxx.portal"
         minSdk = catalogMinSdk
         targetSdk = catalogCompileSdk
-        versionCode = 20
+        versionCode = 21
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
