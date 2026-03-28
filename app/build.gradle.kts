d7cb9f834089bb6ff1b45d3cf03a6a280ff7a930
xXMRK888YTXx <xxmrk888ytxx.rlc@gmail.com>
2026-03-28 23:03:16 +0300

    [Chore] Increment versionCode to 27
    Updated the `versionCode` from 26 to 27 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.xxmrk888ytxx.portal"
         minSdk = catalogMinSdk
         targetSdk = catalogCompileSdk
-        versionCode = 26
+        versionCode = 27
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
