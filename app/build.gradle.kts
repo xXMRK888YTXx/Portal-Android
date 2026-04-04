cee26595e28f526e9cb94da0bf948eb40d8343c1
xXMRK888YTXx <xxmrk888ytxx.rlc@gmail.com>
2026-04-04 22:30:39 +0300

    [Chore] Increment version code to 31
    This commit updates the application's version code in the build configuration.
    
    Key changes:
    - **Build Configuration**: Updated `versionCode` from 30 to 31 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.xxmrk888ytxx.portal"
         minSdk = catalogMinSdk
         targetSdk = catalogCompileSdk
-        versionCode = 30
+        versionCode = 31
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
