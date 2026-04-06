f71967c58881b05007003bd3ae97aa8f62dbbd89
xXMRK888YTXx <xxmrk888ytxx.rlc@gmail.com>
2026-04-06 22:34:50 +0300

    [Chore] Update application versioning
    This commit increments the application version code and updates the version name.
    
    Key changes:
    - **Build Configuration**:
        - Updated `versionCode` from `31` to `32`.
        - Updated `versionName` from `"1.0"` to `"Eva.0"`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.xxmrk888ytxx.portal"
         minSdk = catalogMinSdk
         targetSdk = catalogCompileSdk
-        versionCode = 31
-        versionName = "1.0"
+        versionCode = 32
+        versionName = "Eva.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
